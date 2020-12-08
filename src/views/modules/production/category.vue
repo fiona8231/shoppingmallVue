629d606f533a3310b23ea17f3ea05f64ca3fd619
Yue <fionaxuyue8231@gmail.com>
2020-12-08 23:34:59 +0100

    ElementUI Init

--- src/views/modules/production/category.vue
+++ src/views/modules/production/category.vue
@@ -1,17 +1,54 @@
-
 <template>
-
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
 </template>
 
 <script>
-//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
-//例如：import 《组件名称》 from '《组件路径》';
 
 export default {
-//import引入的组件需要注入到对象中才能使用
+
 components: {},
 data() {
-//这里存放数据
+return {
+        data: [{
+          label: '一级 1',
+          children: [{
+            label: '二级 1-1',
+            children: [{
+              label: '三级 1-1-1'
+            }]
+          }]
+        }, {
+          label: '一级 2',
+          children: [{
+            label: '二级 2-1',
+            children: [{
+              label: '三级 2-1-1'
+            }]
+          }, {
+            label: '二级 2-2',
+            children: [{
+              label: '三级 2-2-1'
+            }]
+          }]
+        }, {
+          label: '一级 3',
+          children: [{
+            label: '二级 3-1',
+            children: [{
+              label: '三级 3-1-1'
+            }]
+          }, {
+            label: '二级 3-2',
+            children: [{
+              label: '三级 3-2-1'
+            }]
+          }]
+        }],
+        defaultProps: {
+          children: 'children',
+          label: 'label'
+        }
+      };
 return {
 
 };
