b1da5848205f2c72f2150321bbdcf0f13905aa71
fiona8231 <9246068@qq.com>
2020-12-10 20:41:43 +0100

    Menu Deltele function finished

--- src/views/modules/production/category.vue
+++ src/views/modules/production/category.vue
@@ -1,13 +1,29 @@
 <template>
-  <el-tree :data="menus" :props="defaultProps" :expand-on-click-node="false" show-checkbox
-   node-key="catId">
+  <el-tree
+    :data="menus"
+    :props="defaultProps"
+    :expand-on-click-node="false"
+    show-checkbox
+    node-key="catId"
+    :default-expanded-keys="expandedKey"
+  >
     <span class="custom-tree-node" slot-scope="{ node, data }">
       <span>{{ node.label }}</span>
       <span>
-        <el-button v-if="node.level <=2" type="text" size="mini" @click="() => append(data)">
+        <el-button
+          v-if="node.level <= 2"
+          type="text"
+          size="mini"
+          @click="() => append(data)"
+        >
           Append
         </el-button>
-        <el-button v-if="node.childNodes.length == 0" type="text" size="mini" @click="() => remove(node, data)">
+        <el-button
+          v-if="node.childNodes.length == 0"
+          type="text"
+          size="mini"
+          @click="() => remove(node, data)"
+        >
           Delete
         </el-button>
       </span>
@@ -24,6 +40,7 @@ export default {
       defaultProps: {
         children: "children",
         label: "name",
+        expandedKey:[],
       },
     };
     return {};
@@ -44,17 +61,41 @@ export default {
         // })
       }).then(({ data }) => {
         console.log("成功获取菜单数据", data.data);
+
         this.menus = data.data;
       });
     },
-   
-    append(data) {
-         console.log("append",data)
-      },
 
-      remove(node, data) {
-         console.log("remove", node, data)
-      },
+    append(data) {
+      console.log("append", data);
+    },
+
+    remove(node, data) {
+      var ids = [data.catId];
+      this.$confirm(`是否确认删除【${data.name}】菜单`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      }).then(() => {
+        this.$http({
+          url: this.$http.adornUrl("/production/category/delete"),
+          method: "post",
+          data: this.$http.adornData(ids, false),
+        }).then(({ data }) => {
+          this.$message({
+            message: "菜单删除成功",
+            type: "success",
+          });
+          console.log("删除成功");
+          //删除成功，刷新出新的菜单
+          this.getMeuns();
+          //设置默认删除刷新完展开的菜单
+          this.expandedKey = [node.parent.data.catId];
+        });
+      });
+
+      console.log("remove", node, data);
+    },
   },
   watch: {},
 
