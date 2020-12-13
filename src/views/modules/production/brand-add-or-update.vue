288c060aab2688553653ae13c58342be123856b7
fiona8231 <9246068@qq.com>
2020-12-13 11:16:21 +0100

    Brand image upload finished

--- src/views/modules/production/brand-add-or-update.vue
+++ src/views/modules/production/brand-add-or-update.vue
@@ -15,7 +15,8 @@
         <el-input v-model="dataForm.name" placeholder="品牌名"></el-input>
       </el-form-item>
       <el-form-item label="品牌logo地址" prop="logo">
-        <el-input v-model="dataForm.logo" placeholder="品牌logo地址"></el-input>
+        <!-- <el-input v-model="dataForm.logo" placeholder="品牌logo地址"></el-input> -->
+         <singleUpload v-model="dataForm.logo"></singleUpload> 
       </el-form-item>
       <el-form-item label="介绍" prop="descript">
         <el-input v-model="dataForm.descript" placeholder="介绍"></el-input>
@@ -45,7 +46,9 @@
 </template>
 
 <script>
+import singleUpload from "@/components/upload/singleUpload"
 export default {
+  components:{singleUpload:singleUpload}, 
   data() {
     return {
       visible: false,
